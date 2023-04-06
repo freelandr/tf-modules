24f4fba4fee630a712dd9fee0e7e394f2c6e0b26
Richard.Freeland <Richard.Freeland-CIC@ibm.com>
2023-04-06 09:47:54 +0100

    update db name

--- data-storage/mysql/main.tf
+++ data-storage/mysql/main.tf
@@ -1,10 +1,10 @@
 resource "aws_db_instance" "example" {
-  identifier_prefix = "${var.cluster_name}-terraform-up-and-running"
+  identifier_prefix = "${var.cluster_name}-tf"
   engine = "mysql"
   allocated_storage = 10
   instance_class = var.instance_class
   skip_final_snapshot = true
-  db_name = "${var.cluster_name}_example_db"
+  db_name = "${var.cluster_name}-db"
 
   username = var.db_username
   password = var.db_password
