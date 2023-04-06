efeb8ab04f827957c4057a92a0f93ed676eb9aed
Richard.Freeland <Richard.Freeland-CIC@ibm.com>
2023-04-06 09:49:39 +0100

    update db name

--- data-storage/mysql/main.tf
+++ data-storage/mysql/main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "example" {
   allocated_storage = 10
   instance_class = var.instance_class
   skip_final_snapshot = true
-  db_name = "${var.cluster_name}-db"
+  db_name = "${var.cluster_name}DB"
 
   username = var.db_username
   password = var.db_password
